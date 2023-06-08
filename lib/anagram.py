fb1a9b0edfd8998fed50b229ab745df331984102
Cherise Foster <cherise.foster@outlook.com>
2023-06-08 12:13:26 -0600

    completed tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, letters):
+        anagram = []
+        for letter in letters:
+            if len(letter) == len(self.word) and letter.lower() != self.word:
+                if sorted(letter.lower()) == sorted(self.word):
+                    anagram.append(letter)
+        return anagram
+
+                  
+# class Anagram:
+#     def __init__(self, word):
+#         self.word = sorted(word.lower())
+
+#     def match(self, anagrams):
+#         return [anagram for anagram in anagrams if sorted(anagram.lower()) == self.word and anagram.lower() != self.word]
+
